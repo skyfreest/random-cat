3762df588ca5078975ba440b69e326e3b6bd8705
skyfreest <urahare85@gmail.com>
2023-07-22 13:03:09 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,49 @@
+import { NextPage } from "next";
+import { useEffect,useState } from "react";
+import { type } from "os";
+
+const IndexPage = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading ,setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url);
+      setLoading(false);
+    });
+  },
+  []);
+
+  const handleClick = async () => {
+  setLoading(true);
+  const newImage = await fetchImage();
+  setImageUrl(newImage.url);
+  setLoading(false);
+};
+return (
+  <div>
+    <button onClick={handleClick}>ほかの猫</button>
+    <div>{loading || <img src={imageUrl}/>}</div>
+  </div>
+);
+
+  return <div>{loading || <img src={imageUrl}/>}</div>;
+};
+
+export default IndexPage
+
+
+type Image = {
+  url: string;
+};
+
+const fetchImage = async ():Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
+};
+/**
+ * ↓FetcImageテスト用
+ * fetchImage();
+ */
